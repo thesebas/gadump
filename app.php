f4168c8787958ca1c214a2f0e9dd5ad1f4eb9221
thesebas <thesebas@thesebas.net>
2015-12-01 20:33:36 +0100

    day by day, get domain, exclude some general profiles

--- app.php
+++ app.php
@@ -36,34 +36,62 @@ $accounts = $service->management_accounts->listManagementAccounts();
 
 $profiles = $service->management_profiles->listManagementProfiles('~all', '~all');
 
-$getData = function ($id, $start, $maxres = 1000) use ($service) {
-    $data = $service->data_ga->get("ga:{$id}", '2015-06-08', '2015-09-06', 'ga:pageviews', [
+$getData = function ($id, $start, $maxres = 1000, $dateFrom, $dateTo) use ($service) {
+    $data = $service->data_ga->get("ga:{$id}", $dateFrom, $dateTo, 'ga:pageviews', [
         'start-index' => $start,
         'max-results' => $maxres,
-        'dimensions' => 'ga:pagePath',
+        'dimensions' => 'ga:pagePath,ga:hostname',
         'sort' => '-ga:pageviews'
     ]);
 
     $rows = $data->getRows();
     return [$rows, count($rows) >= $maxres];
 };
+// mock
+$_getData = function ($id, $start, $maxres = 1000, $dateFrom, $dateTo) {
+    echo "$id, $start, $maxres, $dateFrom, $dateTo", PHP_EOL;
+    return [[], false];
+};
+
+$excludeIds = [
+    6660064,
+    105970498,
+    12769295,
+    102564109,
+];
+$startDate = strtotime('2015-06-08');
+$endDate = strtotime('2015-09-06');
 
 foreach ($profiles as $profile) {
     /** @var $profile Google_Service_Analytics_Profile */
 
-    echo $profile->getName(), PHP_EOL;
+//    echo $profile->getName(), PHP_EOL;
 
-    $perPage = 1000;
-    $from = 1;
+    $perPage = 10000;
 
+
+    $profileId = $profile->getId();
+
+    if (in_array($profileId, $excludeIds)) {
+        continue;
+    }
+
+    $subStartDate = $startDate;
     do {
-        list($rows, $hasMore) = $getData($profile->getId(), $from, $perPage);
-        foreach ($rows as $row) {
-            list($path, $pv) = $row;
+        $subEndDate = strtotime('+1day', $subStartDate);
+        $from = 1;
+        do {
+            list($rows, $hasMore) = $getData($profileId, $from, $perPage, date('Y-m-d', $subStartDate), date('Y-m-d', $subEndDate));
+            if (is_array($rows)) {
+                foreach ($rows as $row) {
+                    list($path, $domain, $pv) = $row;
 
-            echo "\t", $path, "\t", $pv, PHP_EOL;
-        }
+                    echo $domain, "\t", $path, "\t", $pv, "\t", date('Y-m-d', $subStartDate), PHP_EOL;
+                }
+            }
 
-        $from += $perPage;
-    } while ($hasMore);
+            $from += $perPage;
+        } while ($hasMore);
+        $subStartDate = $subEndDate;
+    } while ($subEndDate <= $endDate);
 }
